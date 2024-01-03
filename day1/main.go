7d93cb3761a714ccf7742c8082229bc1811b25b0
Ádran Farias Carnavale <adran.carnavale@gmail.com>
2024-01-03 17:22:34 -0300

    aoc 2023 day1 resolution

--- day1/main.go
+++ day1/main.go
@@ -1,15 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+)
 
 func main() {
-	print()
+	total, err := trebuchet()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(total)
 }
 
-func print() string {
-	res := "day1"
+func trebuchet() (int, error) {
+	var coordinates []int
+	var total int
 
-	fmt.Println(res)
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		return 0, err
+	}
 
-	return res
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	re := regexp.MustCompile(`\d`)
+
+	for scanner.Scan() {
+		numericChars := re.FindAllString(scanner.Text(), -1)
+		firstDigit := numericChars[0]
+		lastDigit := numericChars[len(numericChars)-1]
+
+		coordinate := fmt.Sprintf("%s%s", firstDigit, lastDigit)
+
+		val, err := strconv.Atoi(coordinate)
+		if err != nil {
+			return 0, err
+		}
+
+		coordinates = append(coordinates, val)
+	}
+
+	for _, c := range coordinates {
+		total += c
+	}
+
+	return total, nil
 }
